9a152231799b89e0d4ac20cebf97f976efbe822b
humphreh <humhu@cmu.edu>
2014-10-28 10:58:03 -0400

    Implemented lazy-lookup Gibbs field classes.

--- include/intelligent/GibbsField.h
+++ include/intelligent/GibbsField.h
@@ -1,22 +1,125 @@
 #ifndef _GIBBS_FIELD_H_
 #define _GIBBS_FIELD_H_
 
+#include <memory>
+#include <vector>
+#include <map>
+
 namespace intelligent {
 
 	class GibbsField;
-	
-	class GibbsNode {
-	public:
+	class GibbsVariable;
 
+	/*! \note This Gibbs field implementation uses a graph with runtime edge lookup for speed.
+	 * I expect the fields to be copied a lot, so we trade off sampling speed for copy
+	 * construction complexity. Specifically, variables (nodes) and potentials (edges) do
+	 * not store pointers to each other. All operations requiring this information depend
+	 * on ID-based lookup from the owning field.
+	 */
+	
+	/*! \brief Superclass for all potentials in a Gibbs field. Equivalent to an
+	 * edge in the Gibbs graph. */
+	class GibbsPotential {
+	public:
+		
+		typedef std::shared_ptr<GibbsPotential> Ptr;
+
+		/*! \brief The field this potential belongs to. */
 		const GibbsField& field;
 
-		GibbsNode( const GibbsField& field );
+		/*! \brief This potential's field-unique ID. */
+		const unsigned int id;
+
+		/*! \brief Construct a new potential with a specified ID and specified variable IDs. */
+		GibbsPotential( const GibbsField& _field, unsigned int _id,
+						const std::vector<unsigned int>& _variableIDs );
+
+		/*! \brief Copy a potential but assign it to a different field. */
+		GibbsPotential( const GibbsField& _field, const GibbsPotential& other );
+
+		/*! \brief Return a smart pointer to a copy-constructed potential. */
+		virtual GibbsPotential::Ptr Clone( const GibbsField& _field ) = 0;
+		
+		/*! \brief Return the exponent potential for this potential for
+		 * the current values of its relevant variables. */
+		virtual double CalculatePotential() = 0;
+
+		// NOTE No way around this typedef hack
+		/*! \brief Get the variables that this potential operates over. */
+		std::vector < std::shared_ptr<GibbsVariable> > GetClique() const;
+
+	private:
+
+		/*! \brief The IDs corresponding to this potential's variables. */
+		std::vector<unsigned int> variableIDs;
+
+	};
+
+	/*! \brief Superclass for all variables in a Gibbs field. Equivalent to a
+	 * node in the Gibbs graph. */
+	class GibbsVariable {
+	public:
+
+		typedef std::shared_ptr<GibbsVariable> Ptr;
+
+		/*! \brief The field this variable belongs to. */
+		const GibbsField& field;
+
+		/*! \brief This variable's field-unique ID. */
+		const unsigned int id;
+
+		/*! \brief Construct a new variable with specified ID and potential IDs. */
+		GibbsVariable( const GibbsField& _field, unsigned int _id,
+					   const std::vector<unsigned int>& _potentialIDs );
+
+		/*! \brief Copy a variable but assign it to a different field. */
+		GibbsVariable( const GibbsField& _field, const GibbsVariable& other );
+
+		/*! \brief Return a smart poitner to a copy-constructed variable. */
+		virtual GibbsVariable::Ptr Clone( const GibbsField& _field ) = 0;
+		
+		/*! \brief Sample this variable proportional to its potentials. */
+		virtual void Sample() = 0;
+
+		/*! \brief Calculate the product of exponentiated potentials connected
+		 * to this variable. */
+		double CalculatePotential();
+
+		/*! \brief Get the potentials that operate on this variable. */
+		std::vector<GibbsPotential::Ptr> GetPotentials() const;
+
+	private:
+
+		/*! \brief The IDs corresponding to this variable's potentials. */
+		const std::vector<unsigned int> potentialIDs;
+		
 	};
 	
 	class GibbsField {
 	public:
 
 		GibbsField();
+		GibbsField( const GibbsField& other );
+
+		void AddVariable( const GibbsVariable::Ptr& var );
+		void AddPotential( const GibbsPotential::Ptr& pot );
+
+		GibbsVariable::Ptr GetVariable( unsigned int id ) const;
+		std::vector<GibbsVariable::Ptr> GetVariables() const;
+
+		GibbsPotential::Ptr GetPotential( unsigned int id ) const;
+		std::vector<GibbsPotential::Ptr> GetPotentials() const;
+		
+	private:
+
+		typedef std::map <unsigned int, GibbsVariable::Ptr> VariableMap;
+		typedef std::map <unsigned int, GibbsPotential::Ptr> PotentialMap;
+		
+		/*! \brief Map from variable IDs to pointers. */
+		VariableMap variables;
+
+		/*! \brief Map from potential IDs to pointers. */
+		PotentialMap potentials;
 		
 	};
 	
--- src/GibbsField.cpp
+++ src/GibbsField.cpp
@@ -1,7 +1,123 @@
 #include "intelligent/GibbsField.h"
 
-namespace intelligent {
+#include <boost/foreach.hpp>
 
+#include <stdexcept>
+#include <sstream>
+#include <cmath>
+
+namespace intelligent {
+	
+	GibbsPotential::GibbsPotential( const GibbsField& _field, unsigned int _id,
+									const std::vector<unsigned int>& _variableIDs ) :
+		field( _field ),
+		id( _id ),
+		variableIDs( _variableIDs ) {}
+
+	GibbsPotential::GibbsPotential( const GibbsField& _field,
+									const GibbsPotential& other ) :
+		field( _field ),
+		id( other.id ),
+		variableIDs( other.variableIDs ) {}
+
+	std::vector<GibbsVariable::Ptr> GibbsPotential::GetClique() const {
+		
+		std::vector<GibbsVariable::Ptr> clique;
+		BOOST_FOREACH( unsigned int varID, variableIDs ) {
+			clique.push_back( field.GetVariable( varID ) );
+		}
+		return clique;
+	}
+		
+	GibbsVariable::GibbsVariable( const GibbsField& _field, unsigned int _id,
+								  const std::vector<unsigned int>& _potentialIDs ) :
+		field( _field ),
+		id( _id ),
+		potentialIDs( _potentialIDs ) {}
+
+	GibbsVariable::GibbsVariable( const GibbsField& _field, const GibbsVariable& other ) :
+		field( _field ),
+		id( other.id ),
+		potentialIDs( other.potentialIDs ) {}
+
+	double GibbsVariable::CalculatePotential() {
+
+		double expSum = 0.0;
+		std::vector <GibbsPotential::Ptr> potentials = GetPotentials();
+		BOOST_FOREACH( const GibbsPotential::Ptr& pot, potentials ) {
+			expSum += pot->CalculatePotential();
+		}
+
+		return std::exp( expSum );
+	}
+
+	std::vector<GibbsPotential::Ptr> GibbsVariable::GetPotentials() const {
+		
+		std::vector<GibbsPotential::Ptr> potentials;
+		BOOST_FOREACH( unsigned int potID, potentialIDs ) {
+			potentials.push_back( field.GetPotential( potID ) );
+		}
+		return potentials;
+	}
+		
 	GibbsField::GibbsField() {}
+
+	GibbsField::GibbsField( const GibbsField& other ) {
+
+		BOOST_FOREACH( const VariableMap::value_type& item, other.variables ) {
+			GibbsVariable::Ptr var = item.second->Clone( *this );
+			AddVariable( var );
+		}
+
+		BOOST_FOREACH( const PotentialMap::value_type& item, other.potentials ) {
+			GibbsPotential::Ptr pot = item.second->Clone( *this );
+			AddPotential( pot );
+		}
+		
+	}
+
+	void GibbsField::AddVariable( const GibbsVariable::Ptr& var ) {
+		
+		if( variables.count( var->id ) > 0 ) {
+			std::stringstream ss;
+			ss << "Field already has variable with id " << var->id;
+			throw std::runtime_error( ss.str() );
+		}
+		variables[ var->id ] = var;
+	}
+
+	void GibbsField::AddPotential( const GibbsPotential::Ptr& pot ) {
+		
+		if( potentials.count( pot->id ) > 0 ) {
+			std::stringstream ss;
+			ss << "Field already has potential with id " << pot->id;
+			throw std::runtime_error( ss.str() );
+		}
+		potentials[ pot->id ] = pot;
+	}
+
+	GibbsVariable::Ptr GibbsField::GetVariable( unsigned int id ) const {
+		return variables.at( id );
+	}
+
+	GibbsPotential::Ptr GibbsField::GetPotential( unsigned int id ) const {
+		return potentials.at( id );
+	}
+	
+	std::vector<GibbsVariable::Ptr> GibbsField::GetVariables() const {
+		std::vector<GibbsVariable::Ptr> vars( variables.size() );
+		BOOST_FOREACH( const VariableMap::value_type& item, variables ) {
+			vars.push_back( item.second );
+		}
+		return vars;
+	}
+
+	std::vector<GibbsPotential::Ptr> GibbsField::GetPotentials() const {
+		std::vector<GibbsPotential::Ptr> pots( potentials.size() );
+		BOOST_FOREACH( const PotentialMap::value_type& item, potentials) {
+			pots.push_back( item.second );
+		}
+		return pots;
+	}
 	
 }
